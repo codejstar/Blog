66046b6e64c2d51d0d62d7f8b6f5c193b7926752
code jstar <codejstar@gmail.com>
2024-02-08 17:45:52 +0530

    Add map function and return error

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -1,13 +1,24 @@
 import { useParams } from "react-router-dom";
+import articleContent from "./article-content.js"
 
 
 const Article = () => {
   const {name} = useParams();
+  console.log(name);
+  const article = articleContent.find((article)=> article.name === name);
+  // console.log(article.title);
+
+  if(!article) return <h1>Article does not exists.</h1>;
   return (
     <div>
     <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>
-    This is {name} Article.
+     {article.title}
     </h1>
+    {
+      article.content.map((paragraph,index)=> (
+        <p className="mx-auto leading-relaxed text-base mb-4" key={index}>{paragraph}</p>
+      ))
+    }
     </div> 
   )
 }
