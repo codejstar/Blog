477a02e0cafdac8e471942d6dbac9d9df27c5791
code jstar <codejstar@gmail.com>
2024-02-10 12:37:11 +0530

    add article content

---
+++ src/pages/article-content.js
@@ -0,0 +1,22 @@
+const articles = [
+    {
+        index: 0,
+        name: "learn-react",
+        title: "Learn React",
+        thumbnail: "https://images.pexels.com/photos/19910983/pexels-photo-19910983/free-photo-of-brunette-woman-sitting-in-traditional-clothing-in-town.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        content: [
+            `Welcome to React content page`
+        ]
+    },
+    {
+        index: 4,
+        name: "learn-advance-react",
+        title: "Learn-React-react",
+        thumbnail: "https://images.pexels.com/photos/19910983/pexels-photo-19910983/free-photo-of-brunette-woman-sitting-in-traditional-clothing-in-town.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        content: [
+            `Welcome to advacnce react page`
+        ]
+    }
+];
+
+export default articles;
